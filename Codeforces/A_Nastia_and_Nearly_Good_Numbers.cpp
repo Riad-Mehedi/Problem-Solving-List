da8ec2a6bbdbff666751a8aad6c081d6b4896eaf
Riad-Mehedi <129571327+Riad-Mehedi@users.noreply.github.com>
2024-11-28 12:24:59 +0600

    Add files via upload

---
+++ Codeforces/A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define		ll 		long long int
+#define 	ld 		long double
+
+#define 	nl 		"\n"
+#define     new     cout<<'\n'
+#define 	Yes 	cout<<"Yes"<<nl 
+#define 	No 		cout<<"No"<<nl 
+#define 	YES 	cout<<"YES"<<nl 
+#define 	NO 		cout<<"NO"<<nl 
+
+#define 	vi 		vector<int>
+#define 	vll 	vector<ll>
+#define 	pb 		push_back
+
+#define     FAIYAZ  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define     srt(v)  v.begin(),v.end()
+#define     setp(n) fixed << setprecision(n)
+
+#define     debug(x) cout << #x << " : " << x << endl;
+
+#define     cs      cout << "Case " << t << ": ";
+#define     vp(v)   for(auto it:v){cout << it <<' ';}new;
+
+
+bool isPrime(ll n) {
+    if (n <= 1) return false; 
+    if (n <= 3) return true;  
+    if (n % 2 == 0 || n % 3 == 0) return false; 
+
+    for (ll i = 5; i * i <= n; i += 6) { 
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+
+
+
+int main() {
+	ll tc; cin>>tc;
+	while(tc--) {
+		ll a,b; cin>>a>>b;
+        ll prod = a*b;
+
+        if(b==1) NO;
+        else {
+            YES;
+            cout << a << " " << prod << " "<< prod+a << nl;
+        }
+
+	}
+}
